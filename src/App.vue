967001d3822bc49bafc101ffbc42dbb2c768f5ba
Marwan <kamalmarwan855@gmail.com>
2026-03-04 04:36:15 +0200

    Refactor dashboard into modular sections

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <AppLayout>
-    Dashboard Content
+    <div class="grid grid-cols-12 gap-4">
+      <KpiSection />
+      <RightTuners />
+      <LeftCharts />
+      <MiddleCharts />
+    </div>
   </AppLayout>
 </template>
 
 <script setup>
-import AppLayout from './components/layout/AppLayout.vue'
+import AppLayout from "./components/layout/AppLayout.vue";
+import KpiSection from "./components/dashboard/KpiSection.vue";
+import LeftCharts from "./components/dashboard/LeftCharts.vue";
+import MiddleCharts from "./components/dashboard/MiddleCharts.vue";
+import RightTuners from "./components/dashboard/RightTuners.vue";
 </script>
