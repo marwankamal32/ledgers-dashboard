bcffd2dee68f9337bb88e750bc7c5988a2c4d729
Marwan <kamalmarwan855@gmail.com>
2026-03-09 00:19:53 +0200

    Refactor LeftCharts component to improve layout and styling; integrate AreaChartCard and LineChartCard

--- src/components/dashboard/LeftCharts.vue
+++ src/components/dashboard/LeftCharts.vue
@@ -1,13 +1,19 @@
 <template>
-  <!-- ================= LEFT COLUMN (5 cols) ================= -->
-  <div class="col-span-12 lg:col-span-5 space-y-4">
-    <BarChartCard />
+  <div>
+    <div class="bg-[#4499E3] rounded-3xl p-4 flex flex-col lg:flex-row gap-4 w-full">
+      <div class="flex-1">
+        <AreaChartCard title="Cash inflow vs outflow" />
+      </div>
 
-    <AreaChartCard title="Cash inflow vs outflow" />
+      <div class="flex flex-col gap-4 w-full lg:w-1/3">
+        <LineChartCard title="Cash In Bank" />
+        <LineChartCard title="NetFlow" :values="[300, 280, 150, 50, 300, -200, -100, 200, 20, 100, 180]"" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import BarChartCard from './BarChartCard.vue'
 import AreaChartCard from './AreaChartCard.vue'
+import LineChartCard from './LineChartCard.vue'
 </script>
