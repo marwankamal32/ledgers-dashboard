2d4cf4ae58806f1bbf4d81220c164365dfc000eb
Marwan <kamalmarwan855@gmail.com>
2026-03-08 23:45:28 +0200

    Update AppLayout component to enhance styling and layout structure

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="flex min-h-screen bg-gray-100">
-      <AppSidebar />
-      <div class="flex-1">
-        <AppHeader />
-        <main class="p-6">
-          <slot />
-        </main>
-      </div>
+  <div
+    class="flex min-h-screen"
+    style="background: #D7E3F0"
+  >
+    <AppSidebar class="hidden md:flex" />
+    <div class="flex-1 md:ml-20">
+      <AppHeader />
+      <main class="p-4 sm:p-6">
+        <slot />
+      </main>
     </div>
-  </template>
-  
-  <script setup>
-  import AppSidebar from './AppSidebar.vue'
-  import AppHeader from './AppHeader.vue'
-  </script>
+  </div>
+</template>
+
+<script setup>
+import AppSidebar from "./AppSidebar.vue";
+import AppHeader from "./AppHeader.vue";
+</script>
