d9179b4698b86dce893d27c6721b4f9555857e56
Marwan <kamalmarwan855@gmail.com>
2026-03-09 00:20:47 +0200

    Import PrimeIcons CSS for enhanced icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import Chart from 'primevue/chart'
+import 'primeicons/primeicons.css'
 import './style.css'
 import App from './App.vue'
 
