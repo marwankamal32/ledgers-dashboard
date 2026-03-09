3b89ce157f21ffae02051b75addb1c009a8609ab
Marwan <kamalmarwan855@gmail.com>
2026-03-09 04:16:58 +0200

    Update App.vue layout for improved responsiveness and grid structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <AppLayout>
-    <div class="grid grid-cols-12 gap-4">
-      <KpiSection />
-      <RightTuners />
-      <LeftCharts />
-      <MiddleCharts />
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-4 gap-y-3 auto-rows-[min-content]">
+      <KpiSection class="col-span-12 lg:col-span-7" />
+      <RightTuners class="col-span-12 lg:col-span-5 lg:row-span-3" />
+      <MiddleCharts class="col-span-12 lg:col-span-7" />
+      <LeftCharts class="col-span-12 lg:col-span-7" />
+      
     </div>
   </AppLayout>
 </template>
